345f1e85b5b27395bdec8b0bd5d213b0ae4da8fa
bilyyZ1 <briliando771@gmail.com>
2025-07-29 21:37:19 +0700

    4.py

--- 4.py
+++ 4.py
@@ -8,18 +8,18 @@ print("""
 ╚══════════════════════════════════════════════╝
 """)
 
-# Input
+# Input user
 judul = input("\033[93m[+] Judul Deface: \033[0m")
 pesan = input("\033[93m[+] Pesan Utama: \033[0m")
 logo_url = input("\033[93m[+] URL Logo Gambar: \033[0m")
 musik_url = input("\033[93m[+] URL Musik Latar (MP3): \033[0m")
 bg_url = input("\033[93m[+] URL Background (gambar/video/gif): \033[0m")
-nama_file = input("\033[93m[+] Nama File Output (contoh: defaced.html): \033[0m")
+nama_file = input("\033[93m[+] Nama File Output (contoh: deface.html): \033[0m")
 
 # Cek jenis background
 is_video = any(ext in bg_url.lower() for ext in ['.mp4', 'youtube.com', 'youtu.be'])
 
-# HTML Generator
+# HTML generator
 if is_video:
     bg_html = f"""
   <video autoplay muted loop id="bgvid" style="position:fixed;right:0;bottom:0;min-width:100%;min-height:100%;z-index:-1;">
@@ -36,7 +36,7 @@ else:
   </style>
     """
 
-# Gabungkan semua
+# Gabung semua HTML
 html_code = f"""<!DOCTYPE html>
 <html lang="en">
 <head>
